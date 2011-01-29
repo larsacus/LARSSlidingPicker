b7633c4c3aef0489e5c0bbc29cd08bba4ee92e97
Lars <lars@drinkandapple.com>
2011-01-29 15:26:47 -0600

    Added MIT License

--- SlidingPicker.m
+++ SlidingPicker.m
@@ -3,8 +3,30 @@
 //  RealLED
 //
 //  Created by Lars Anderson on 11/9/10.
-//  Copyright 2010 Lars Anderson. All rights reserved.
+
+// The MIT License
 //
+// Copyright (c) 2010-2011 Lars Anderson
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
 #define ANIMATION_DURATION 0.15
 
 #import "SlidingPicker.h"
@@ -32,14 +54,6 @@
 -(id)initWithHeight:(CGFloat)height{
 	return [self initWithFrame:CGRectMake(0.0f, 460.0f, 320.0f, height)];
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code.
-}
-*/
 
 -(void)togglePickerBeforeToggling:(SlidingPicker *)otherPicker withAdditionalView:(UIView *)view{
 	
